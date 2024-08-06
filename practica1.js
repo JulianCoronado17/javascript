045347d66055990243dee94f322ea17121c473f6
JulianCoronado17 <juliancoronado758@gmail.com>
2024-08-06 12:55:08 -0500

    feat: :sparkles: Practica
    @Julian

--- practica1.js
+++ practica1.js
@@ -1,1 +1,11 @@
-console.log("Mamaguevos",8+5)
+const ternure ={
+    edad:68,
+    Hobbie:"Preparacion de churros",
+    titulo:"Diosa Potaxie",
+    nPatas:3
+}
+
+const ternura68 = ternure;
+ternura68.profesion = "Psicologa68"
+console.log(ternura68)
+console.log(ternure)
